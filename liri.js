d208a8ffb33f7279512fdc1f34115c59f44a6d7d
Fintech79 <abdelrahman.faisal@gmail.com>
2018-02-14 22:09:21 -0800

    LiriWorks

--- liri.js
+++ liri.js
@@ -22,108 +22,70 @@ var spotify = new Spotify({
 
 
 
-if (process.argv[2]) {
-  var userinput = process.argv[2];
-}
+ 
+var userinput = process.argv[2];
+console.log(userinput);
 
 if (userinput === "my-tweets"){ 
-	var params = {screen_name: 'AbdelFaisal'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+  var params = {screen_name: 'AbdelFaisal'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
     
-    for (var i = tweets.length - 1; i >= 0; i--) {
-    	
-    	console.log(tweets[i].text)
-    }
+        for (var i = tweets.length - 1; i >= 0; i--) {
+          console.log(tweets[i].text)
+        }
+      }
+  });
+} else if( userinput === "spotify-this-song"){
+  var nodeArg = process.argv;
+  
+  var songName = "";
+
+  for (var i = 3; i < nodeArg.length; i++) {
+      songName += nodeArg[i] + " ";
   }
-});
 
-	
-}
-else if( userinput === "spotify-this-song"){
-	var nodeArg = process.argv;
-
-	
-	var songName = "";
-
-	
-	for (var i = 3; i < nodeArg.length; i++) {
-
-	   
-	    songName += nodeArg[i] + " ";
-
-
-	}
-	spotify.search({ type: 'track', query: songName }, function(err, data) {
- 		if (err) {
-    		return console.log('Error occurred: ' + err);
-  		}
-  		for (var i = 0; i < data.tracks.items.length; i++) {
-  			// console.log(data.tracks.items)
-  			console.log(data.tracks.items[i].album.name); 
-		    console.log(data.tracks.items[i].album.href); 
-  		}
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        // console.log(data.tracks.items)
+        console.log(data.tracks.items[i].album.name); 
+        console.log(data.tracks.items[i].album.href); 
+      }
         // console.log(JSON.stringify(data, null, 2))
-		// console.log(data.tracks.items[0].album.artists[0].name); 
-		// console.log(data.tracks.items[0].album.artists[0].href); 
-		// console.log(data.tracks.items[0].album.artists[0].songName);
-		// console.log(data.tracks.items[0].album.artists.name.popularity);
-		// console.log(data.tracks.items[0].album);
+    // console.log(data.tracks.items[0].album.artists[0].name); 
+    // console.log(data.tracks.items[0].album.artists[0].href); 
+    // console.log(data.tracks.items[0].album.artists[0].songName);
+    // console.log(data.tracks.items[0].album.artists.name.popularity);
+    // console.log(data.tracks.items[0].album);
 
-	});
+  });
+} else if(userinput==="movie-this"){
+  console.log("chicken")
+  var request = require("request");
 
-
-
+    request("http://www.omdbapi.com/?apikey=40e9cece&t=Mr+Nobody", function(error, response, body) {
+      
+      if (!error && response.statusCode === 200) {
+        // Then we print out the imdbRating
+        console.log("The movie's rating is: " +JSON.parse(body).imdbRating);
+      }
+  });
 }
 
+  else if(userinput==="do-what-it-says"){
+      var fs = require("fs");
+      fs.readFile("random.txt", "utf8", function(error, data) {
 
-	// var imdbRating = ""
+      if (error) {
+           return console.log(error);
+     }
+      console.log(data);
+      var dataArr = data.split(",");
+      console.log(dataArr);
 
- // else if(userinput==="movie-this"){
-
-//  	var request = require("request");
-
-
-// request("http://www.omdbapi.com/apikey.aspx?VERIFYKEY=ed6cd174-fc10-4855-b434-1550c35f6879", function(error, response, body) {
-
-//   // If there were no errors and the response code was 200 (i.e. the request was successful)...
-//   if (!error && response.statusCode === 200) {
-
-//     // Then we print out the imdbRating
-//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//   }
-// });
-
-
-
-
-
-
-
-// else if(userinput==="do-what-it-says"){
-
-// 	var fs = require("fs");
-
-
-// fs.readFile("do-what-it-says", "utf8", function(error, data) {
-
-//     // If the code experiences any errors it will log the error to the console.
-//     if (error) {
-//         return console.log(error);
-//     }
-
-//     // We will then print the contents of data
-//     console.log(data);
-
-//     // Then split it by commas (to make it more readable)
-//     var dataArr = data.split(",");
-
-//     // We will then re-display the content as an array for later use.
-//     console.log(dataArr);
-
-// });
-
-
-
-
-
+  });
+}
+console.log("chicken")
